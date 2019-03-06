e22179a2fee9fe3daf38e96a1768603fb3da1955
David <david@biomance.com>
2019-03-05 23:01:40 -0600

    Update AppStyles.tsx
    Minor style update.

--- AppStyles.tsx
+++ AppStyles.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const AppStyles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         alignItems: 'center',
         backgroundColor: '#F5FCFF'
     },
